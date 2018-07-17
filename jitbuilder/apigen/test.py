5a96bc64afee980a5d74013e1e21e3a754195df4
Leonardo Banderali <leonardo2718@protonmail.com>
2018-07-17 09:16:59 -0400

    Add test to validate JitBuilder API
    Validation is done agains the API format schema.

--- jitbuilder/apigen/test.py
+++ jitbuilder/apigen/test.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
-from jsonschema import Draft6Validator, validate
+from jsonschema import Draft6Validator, validate, ValidationError
+import os
+import sys
 import json
 import unittest
 
@@ -30,6 +32,30 @@ class ValidateSchemas(unittest.TestCase):
         with open('schema/api-class.schema.json') as f:
             schema = json.loads(f.read())
             Draft6Validator.check_schema(schema)
+
+    def test_bad_apis(self):
+        bad_apis_dir = 'test/bad_api'
+        schema = {}
+        with open('schema/api.schema.json') as f:
+            schema = json.loads(f.read())
+        for filename in os.listdir(bad_apis_dir):
+            path = os.path.join(bad_apis_dir, filename)
+            if sys.version_info >= (3,4):
+                # if Python version is >= 3.4, we can use `self.subTest()`
+                with self.subTest(filename), open(path) as f, self.assertRaises(ValidationError):
+                        validate(json.loads(f.read()), schema)
+            else:
+                with open(path) as f, self.assertRaises(ValidationError):
+                        validate(json.loads(f.read()), schema)
+
+class ValidateJitBuilderAPI(unittest.TestCase):
+
+    def test_jitbuilder_api(self):
+        schema = {}
+        with open('schema/api.schema.json') as f:
+            schema = json.loads(f.read())
+        with open('jitbuilder.api.json') as f:
+            validate(json.loads(f.read()), schema)
 
 if __name__ == '__main__':
     unittest.main()
