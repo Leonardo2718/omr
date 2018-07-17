64b773019b1cb557213470bf2272f5b94635dfb3
Leonardo Banderali <leonardo2718@protonmail.com>
2018-07-17 09:13:19 -0400

    Move schemas to their own directory

--- jitbuilder/apigen/test.py
+++ jitbuilder/apigen/test.py
@@ -7,27 +7,27 @@ import unittest
 class ValidateSchemas(unittest.TestCase):
 
     def test_api_schema(self):
-        with open('api.schema.json') as f:
+        with open('schema/api.schema.json') as f:
             schema = json.loads(f.read())
             Draft6Validator.check_schema(schema)
 
     def test_api_type_schema(self):
-        with open('api-type.schema.json') as f:
+        with open('schema/api-type.schema.json') as f:
             schema = json.loads(f.read())
             Draft6Validator.check_schema(schema)
 
     def test_api_field_schema(self):
-        with open('api-field.schema.json') as f:
+        with open('schema/api-field.schema.json') as f:
             schema = json.loads(f.read())
             Draft6Validator.check_schema(schema)
 
     def test_api_service_schema(self):
-        with open('api-service.schema.json') as f:
+        with open('schema/api-service.schema.json') as f:
             schema = json.loads(f.read())
             Draft6Validator.check_schema(schema)
 
     def test_api_class_schema(self):
-        with open('api-class.schema.json') as f:
+        with open('schema/api-class.schema.json') as f:
             schema = json.loads(f.read())
             Draft6Validator.check_schema(schema)
 
