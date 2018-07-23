526b05b4948466a940d443b3ec818ef685015dca
Leonardo Banderali <leonardo2718@protonmail.com>
2018-07-23 14:11:51 -0400

    Clean up test runner

--- jitbuilder/apigen/test.py
+++ jitbuilder/apigen/test.py
@@ -10,56 +10,56 @@ class ValidateSchemas(unittest.TestCase):
 
     def test_api_schema(self):
         with open('schema/api.schema.json') as f:
-            schema = json.loads(f.read())
+            schema = json.load(f)
             Draft6Validator.check_schema(schema)
 
     def test_api_type_schema(self):
         with open('schema/api-type.schema.json') as f:
-            schema = json.loads(f.read())
+            schema = json.load(f)
             Draft6Validator.check_schema(schema)
 
     def test_api_field_schema(self):
         with open('schema/api-field.schema.json') as f:
-            schema = json.loads(f.read())
+            schema = json.load(f)
             Draft6Validator.check_schema(schema)
 
     def test_api_service_schema(self):
         with open('schema/api-service.schema.json') as f:
-            schema = json.loads(f.read())
+            schema = json.load(f)
             Draft6Validator.check_schema(schema)
 
     def test_api_class_schema(self):
         with open('schema/api-class.schema.json') as f:
-            schema = json.loads(f.read())
+            schema = json.load(f)
             Draft6Validator.check_schema(schema)
 
     def test_bad_apis(self):
         bad_apis_dir = 'test/bad_api'
         schema = {}
         with open('schema/api.schema.json') as f:
-            schema = json.loads(f.read())
+            schema = json.load(f)
         for filename in os.listdir(bad_apis_dir):
             path = os.path.join(bad_apis_dir, filename)
             if sys.version_info >= (3,4):
                 # if Python version is >= 3.4, we can use `self.subTest()`
                 with self.subTest(filename), open(path) as f, self.assertRaises(ValidationError):
-                        validate(json.loads(f.read()), schema)
+                        validate(json.load(f), schema)
             else:
                 with open(path) as f, self.assertRaises(ValidationError):
-                        validate(json.loads(f.read()), schema)
+                        validate(json.load(f), schema)
 
     def test_minimal_api(self):
         with open('schema/api.schema.json') as schema, open('test/minimal_api.json') as api:
-            validate(json.loads(api.read()), json.loads(schema.read()))
+            validate(json.load(api), json.load(schema))
 
 class ValidateJitBuilderAPI(unittest.TestCase):
 
     def test_jitbuilder_api(self):
         schema = {}
         with open('schema/api.schema.json') as f:
-            schema = json.loads(f.read())
+            schema = json.load(f)
         with open('jitbuilder.api.json') as f:
-            validate(json.loads(f.read()), schema)
+            validate(json.load(f), schema)
 
 if __name__ == '__main__':
     unittest.main()
